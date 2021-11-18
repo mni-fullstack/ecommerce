35f02e71bd913685efc229bc42bd2fff7fc3d919
Muhamad Nizar Iqbal <mni@Modern15A11SBf328dac1.local>
2021-11-18 07:16:41 +0700

    Add frontend, add header and footer

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Header from './components/header';
+import Footer from './components/footer'
+
+function App() {
+  return (
+    <>
+      <Header></Header>
+      <main>
+        <h1>welcome to my ecommerce</h1>
+      </main>
+      <Footer></Footer>
+    </>
+  );
+}
+
+export default App;
